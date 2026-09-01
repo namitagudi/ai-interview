a9f14d3fb025424af83971753b4d2a76215155f4
namitagudi <namitaa.gudi@gmail.com>
2026-09-01 14:25:02 +0530

    Update state.py

--- graph/state.py
+++ graph/state.py
@@ -1,1 +1,15 @@
+from typing import List
 
+class InterviewState:
+    def __init__(self):
+        self.sessionid = ""
+        self.candidatename = ""
+        self.askedquestions = []      
+        self.currentquestion = ""
+        self.currenttranscript = ""    
+        self.techscore = 0.0
+        self.commscore = 0.0
+        self.wpm = 0
+        self.verifierscore = 0.0
+        self.perfectedphrase = ""
+        self.quizpassed = False
